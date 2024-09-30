263b3be9ab6e444af02ad680ae2b6a1e7e729ad6
Mounish Vatti <mounishvatti2002@gmail.com>
2024-09-30 12:32:57 +0530

    automatic promotion in overloading

--- src/methodOverloading/MethodOverloading.java
+++ src/methodOverloading/MethodOverloading.java
@@ -3,18 +3,26 @@ package methodOverloading;
 public class MethodOverloading {
 	
 	// method overloading
-	public void m1(int a) {
+	protected void m1(int a) {
+		System.out.print("int-arg - ");
 		System.out.println(a);
 	}
 	
-	public void m1(int a, int b) {
+	protected void m1(int a, int b) {
+		System.out.print("2 int-arg - ");
 		System.out.println(a + ", " +b);
 	}
 	
+	protected void m1(double x) {
+		System.out.print("double-arg - ");
+		System.out.println(x);
+	}
 	
 	public static void main(String[] args) {
 		MethodOverloading m = new MethodOverloading();
 		m.m1(10);
 		m.m1(10, 12);
+		m.m1(10.5);
+		m.m1('a');
 	}
 }

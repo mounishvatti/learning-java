40a2d4393391bdb6c586ad314737cd8c7a6c7775
Mounish Vatti <mounishvatti2002@gmail.com>
2024-09-30 14:00:20 +0530

    string, StringBuffer and var-arg method

--- src/methodOverloading/MethodOverloading.java
+++ src/methodOverloading/MethodOverloading.java
@@ -1,5 +1,7 @@
 package methodOverloading;
 
+import java.util.Arrays;
+
 public class MethodOverloading {
 	
 	// method overloading
@@ -8,9 +10,20 @@ public class MethodOverloading {
 		System.out.println(a);
 	}
 	
-	protected void m1(int a, int b) {
-		System.out.print("2 int-arg - ");
-		System.out.println(a + ", " +b);
+	protected void m1(int... a) { //  This allows the method to accept zero or more int values as arguments.
+		System.out.print("var-arg - ");
+		System.out.println(Arrays.toString(a)); // 
+	}
+	
+	/*
+	 * When you print a using System.out.println(a);
+	 * it will print the memory reference of the array rather than the actual values in the array. 
+	 * If you want to print the values, you should loop through the array or use Arrays.toString().
+	 */
+	
+	protected void m1(float x) {
+		System.out.print("float-arg - ");
+		System.out.println(x);
 	}
 	
 	protected void m1(double x) {
@@ -18,11 +31,34 @@ public class MethodOverloading {
 		System.out.println(x);
 	}
 	
+	protected void m1(long x) {
+		System.out.print("long-arg - ");
+		System.out.println(x);
+	}
+	
+	protected void m2(String s1) {
+		System.out.print("String-arg - ");
+		System.out.println(s1);
+	}
+	
+	protected void m2(StringBuffer s1) {
+		System.out.print("StringBuffer-arg - ");
+		System.out.println(s1);
+	}
+	
 	public static void main(String[] args) {
 		MethodOverloading m = new MethodOverloading();
 		m.m1(10);
 		m.m1(10, 12);
-		m.m1(10.5);
-		m.m1('a');
+		m.m1(10.5f);
+		m.m1(10.5d);
+		m.m1(10l);
+		m.m1('a'); // automatic promotion in overloading
+		
+		// case - 2
+		
+		m.m2("Mounish");
+		m.m2(new StringBuffer("Mounish"));
+		// m.m2(null);
 	}
 }
